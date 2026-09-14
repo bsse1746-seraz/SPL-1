995763e784db047c5595153d1dc78cfd5192e1c8
bsse1746-seraz <bsse1746@iit.du.ac.bd>
2026-09-14 23:49:45 +0600

    3rd commit

--- code.c
+++ code.c
@@ -4,7 +4,7 @@ int main()
 {
     int a, b, c;
     printf("Enter the value of a and b\n");
-    scanf("%d %d    ", &a, &b);
+    scanf("%d %d", &a, &b);
     c = a + b;
     printf("The sum of a and b is %d", c);
     return 0;
